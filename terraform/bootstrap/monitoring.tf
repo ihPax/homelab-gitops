a6e8e9c18792c29b09c94d7872c011eb1e8f36f1
Lorenzo Pavan <l.pavan2575@gmail.com>
2026-06-08 13:31:54 +0000

    Add monitoring stack and Grafana ingress

---
+++ terraform/bootstrap/monitoring.tf
@@ -0,0 +1,66 @@
+resource "helm_release" "kube_prometheus_stack" {
+  name      = "kube-prometheus-stack"
+  namespace = kubernetes_namespace.monitoring.metadata[0].name
+
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = "84.5.0"
+
+  values = [
+    yamlencode({
+      grafana = {
+        enabled = true
+
+        adminPassword = "admin"
+
+        service = {
+          type = "ClusterIP"
+        }
+      }
+
+      prometheus = {
+        prometheusSpec = {
+          retention = "7d"
+
+          storageSpec = {
+            volumeClaimTemplate = {
+              spec = {
+                storageClassName = "local-path"
+                accessModes      = ["ReadWriteOnce"]
+                resources = {
+                  requests = {
+                    storage = "5Gi"
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+
+      alertmanager = {
+        enabled = true
+
+        alertmanagerSpec = {
+          storage = {
+            volumeClaimTemplate = {
+              spec = {
+                storageClassName = "local-path"
+                accessModes      = ["ReadWriteOnce"]
+                resources = {
+                  requests = {
+                    storage = "2Gi"
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    })
+  ]
+
+  depends_on = [
+    kubernetes_namespace.monitoring
+  ]
+}
